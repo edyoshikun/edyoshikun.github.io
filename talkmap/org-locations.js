a77055c04ca79580fdbc0a0e2b1b12527ab5ed41
Eduardo Hirata <edhirata@ucsc.edu>
2019-04-22 23:35:55 -0700

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,16 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
     "London, UK",
     51.5073219,
     -0.1276473
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Santa Cruz, California",
+    36.9741,
+    122.0308
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Baltimore, MA",
+    39.2904, -76.6122
   ]
 ];
